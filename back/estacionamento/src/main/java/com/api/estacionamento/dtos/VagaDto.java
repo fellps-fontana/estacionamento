f1598bfc2bfb9b94f99b75f95c1077a75420f3fd
001-FELIPE.FONTANA-Informatica <FELIPE.FONTANA@cooperalfa.coop.br>
2025-10-24 14:44:27 -0300

    Ajuste usuarios, estacionamento

--- back/estacionamento/src/main/java/com/api/estacionamento/dtos/VagaDto.java
+++
@@ -1,9 +0,0 @@
-package com.api.estacionamento.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class VagaDto {
-    @NotBlank(message = "O número é obrigatório")
-    private Number numero;
-    private String bloco;
-}
